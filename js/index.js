de2a7e07d393eb4bff2a77503deae539d7becccb
Maria Fernanda Arredondo Garcia <mariafarr002@gmail.com>
2026-08-26 00:23:13 -0400

    form and functionality addded

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
   ====================== */
 const createFooter = document.createElement("footer")
 createFooter.style.backgroundColor = "white"
+createFooter.style.marginBottom = "auto"
 createFooter.style.textAlign = "center"
 document.body.appendChild(createFooter)
 
@@ -37,3 +38,36 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i]
   skillsList.appendChild(skill)
 }
+
+/*  =====================
+  Message Form Submit
+  ====================== */
+const messageForm = document.querySelector('form[name="leave_message"]')
+
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault()
+  const uName = event.target.usersName.value
+  const uEmail = event.target.usersEmail.value
+  const uMessage = event.target.usersMessage.value
+  console.log(uName, uEmail, uMessage)
+
+  const messageSection = document.querySelector("#messages")
+  const messageList = messageSection.querySelector("ul")
+  const newMessage = document.createElement("li")
+  newMessage.innerHTML = `<a href= "mailto:${uEmail}">${uName}</a><span> ${uMessage}</span>`
+
+  const removeButton = document.createElement("button")
+  removeButton.innerText = "remove"
+  removeButton.type = "button"
+
+  removeButton.addEventListener("click", function () {
+    const entry = removeButton.parentNode
+    entry.remove()
+  })
+
+
+  newMessage.appendChild(removeButton)
+  messageList.appendChild(newMessage)
+
+  messageForm.reset()
+})
